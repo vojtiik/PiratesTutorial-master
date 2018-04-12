5ee05af68892655b9ca1010976dae4f0e16b0924
Vojtiik <vojtech.mares@justgiving.com>
2018-04-12 21:04:42 +0100

    init

---
+++ workers/unity/Assets/Gamelogic/Pirates/Behaviours/SinkingBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using Improbable.Unity;
+using Improbable.Unity.Visualizer;
+using UnityEngine;
+
+namespace Assets.Gamelogic.Pirates.Behaviours
+{
+    // Add this MonoBehaviour on client workers only
+    [WorkerType(WorkerPlatform.UnityClient)]
+    public class SinkingBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private Animation SinkingAnimation;
+
+        private void OnEnable()
+        {
+        }
+
+        private void OnDisable()
+        {
+        }
+
+        private void VisualiseSinking()
+        {
+            SinkingAnimation.Play();
+        }
+    }
+}
---
+++ workers/unity/Assets/Gamelogic/UI/SplashScreenController.cs
@@ -0,0 +1,61 @@
+﻿using Assets.Gamelogic.Core;
+using Assets.Gamelogic.Utils;
+using Improbable.Unity.Core;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Gamelogic.UI
+{
+    public class SplashScreenController : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject NotReadyWarning;
+        [SerializeField]
+        private Button ConnectButton;
+        [SerializeField]
+        private GameObject Spinner;
+
+
+        public void AttemptToConnect()
+        {
+            // Disable connect button
+            ConnectButton.interactable = false;
+
+            // Hide warning if already shown
+            NotReadyWarning.SetActive(false);
+
+            // Start loading spinner
+            Spinner.SetActive(true);
+
+            AttemptConnection();
+        }
+
+        private void AttemptConnection()
+        {
+            Bootstrap bootstrap = FindObjectOfType<Bootstrap>();
+            if (!bootstrap)
+            {
+                throw new Exception("Couldn't find Bootstrap script on GameEntry in UnityScene");
+            }
+            bootstrap.ConnectToClient();
+
+            // In case the client connection is successful this coroutine is destroyed as part of unloading
+            // the splash screen so ConnectionTimeout won't be called
+            StartCoroutine(TimerUtils.WaitAndPerform(SimulationSettings.ClientConnectionTimeoutSecs, ConnectionTimeout));
+        }
+
+        private void ConnectionTimeout()
+        {
+            if (SpatialOS.IsConnected)
+            {
+                SpatialOS.Disconnect();
+            }
+
+            Spinner.SetActive(false);
+            NotReadyWarning.SetActive(true);
+            ConnectButton.interactable = true;
+        }
+    }
+}
+
